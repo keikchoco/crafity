6cd60e9c1edb0f4f8e3c17aef98bcb26d69c3731
Jeremiah Nueno <jeremiahnueno2017@gmail.com>
2026-07-09 13:10:48 +0800

    update signin

--- app/(admin)/admin/sign-in/[[...sign-in]]/page.tsx
+++ app/(admin)/admin/sign-in/[[...sign-in]]/page.tsx
@@ -4,9 +4,11 @@ import { SignIn } from "@clerk/nextjs"
 
 export default function AdminSignInPage() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-background p-4">
+    <div className="relative flex min-h-screen items-center justify-center bg-background p-4">
       <Backdrop />
-      <SignIn />
+      <div className="relative z-10">
+        <SignIn routing="path" path="/admin/sign-in" />
+      </div>
     </div>
   )
 }
