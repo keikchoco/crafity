f096153617f036375df6b032007f95f7d562b8d5
Jeremiah Nueno <jeremiahnueno2017@gmail.com>
2026-07-09 12:57:00 +0800

    update signin

--- app/(admin)/admin/sign-in/[[...sign-in]]/page.tsx
+++ app/(admin)/admin/sign-in/[[...sign-in]]/page.tsx
@@ -1,8 +1,10 @@
+import { Backdrop } from "@/components/shared/backdrop"
 import { SignIn } from "@clerk/nextjs"
 
 export default function AdminSignInPage() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-background p-4">
+      <Backdrop />
       <SignIn />
     </div>
   )
